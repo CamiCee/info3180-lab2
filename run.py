6bf40c479e1a5873cfe1f6ddc3837d0cb25b655c
Camille Campbell <c.campbell1996@hotmail.com>
2018-02-10 15:27:18 +0000

    Completed Lab

--- run.py
+++ run.py
@@ -4,7 +4,4 @@ import datetime
 from app import app
 app.run(debug=True, host="0.0.0.0", port=8080)
 
-def format_date_joined():
-    now=datetime.datetime.now();
-    date_joined=datetime.date(2018,2,6)
-    print "Joined " + date_joined.strftime("%B, %Y")
+
